e5603cefa01bf608c8c72a5299e826435ffd9da2
Lim Yeong-Bin <dudqls327@naver.com>
2025-10-31 11:47:18 +0900

    2025-10-31 crud / transfer 수정

--- crud/inquiry.py
+++ crud/inquiry.py
@@ -193,13 +193,13 @@ def transfer(db: Session, inquiry_id: int, to_admin_id: int, *, actor_admin_id: 
     obj.assignee_admin_id = to_admin_id
     obj.assigned_at = datetime.now(timezone.utc)
     db.add(obj)
-    _add_history(
-        db,
-        inquiry_id,
-        action="transfer",
-        admin_name=_resolve_admin_name(db, actor_admin_id),
-        details=f"to_admin_id={to_admin_id}",
-    )
+    # _add_history(
+    #     db,
+    #     inquiry_id,
+    #     action="transfer",
+    #     admin_name=_resolve_admin_name(db, actor_admin_id),
+    #     details=f"to_admin_id={to_admin_id}",
+    # )
     db.commit()
     db.refresh(obj)
     return obj
