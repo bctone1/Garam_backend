b3cf7bf01bf81a217f3f09335c3787ab77a36cc3
Lim Yeong-Bin <dudqls327@naver.com>
2026-01-14 17:57:57 +0900

    웹소켓 테스트

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import os, logging, uvicorn
 from contextlib import asynccontextmanager
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
@@ -28,6 +28,21 @@ async def lifespan(app: FastAPI):
         log.info("APScheduler stopped")
 
 app = FastAPI(debug=True, lifespan=lifespan)
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()  # 조건 없이 연결 허용
+    try:
+        while True:
+            data = await websocket.receive_text()  # 어떤 메시지도 수신
+            print(f"받은 메시지: {data}")
+
+            # 그대로 다시 보내기 (echo)
+            await websocket.send_text(f"echo: {data}")
+
+    except WebSocketDisconnect:
+        print("클라이언트 연결 종료")
+
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.mount("/file", StaticFiles(directory=UPLOAD_FOLDER), name="file")
